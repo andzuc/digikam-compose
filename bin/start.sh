9e1fd79b82b182c62a0fe34e200a0366a7dc27ea
Andrea Zuccherelli <andrea@zuccherelli.net>
2024-01-16 22:18:42 +0100

    digikam composition based on linuserver.io image

--- bin/start.sh
+++ bin/start.sh
@@ -2,6 +2,7 @@
 MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "${MYDIR}/.."
 
+export KASMVNC_PORT=3000
 export DIGIKAM_CONFIG=digikam_config
 if ! docker volume inspect "${DIGIKAM_CONFIG}" >/dev/null 2>&1 ; then
     echo docker volume create "${DIGIKAM_CONFIG}"
@@ -10,3 +11,4 @@ fi
 docker volume inspect "${DIGIKAM_CONFIG}"
 
 docker compose up -d
+echo "Open digiKam URL: http://localhost:${KASMVNC_PORT}"
